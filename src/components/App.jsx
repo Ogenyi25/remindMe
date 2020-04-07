33267b94f1bd669afd155a9499eff91c18dd30a7
Ogenyi25 <johnogenyi4real@gmail.com>
2020-04-07 21:49:23 +0100

    started redux setup for the app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
+
 class App extends Component {
     render() {
         return (
-            <div className="App"></div>
+            <div className="App">
+                <div className="title">
+                    REMIND ME
+                </div>
+                <div className="form-inline">
+                    <div className="form-group">
+                        <input type="text" className="form-control"
+                        placeholder="I have to ..."
+                        />
+                    </div>
+                    <button className="btn btn-success">Add Reminder</button>
+                </div>
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
 ReactDOM.render(
-    <div>Remind Me</div>, document.getElementById('root')
+    <App />, document.getElementById('root')
 )
